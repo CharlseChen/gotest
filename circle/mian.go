331b424e4c58c5f865082e350e6603fef3e8b7f7
chen.hao <CharlseChen@users.noreply.github.com>
2023-11-08 19:44:49 +0800

    日志逻辑

--- circle/mian.go
+++ circle/mian.go
@@ -1,14 +1,20 @@
 package main
 
-import (
-	"gotest/circle/test_one"
-	"gotest/circle/test_two"
-)
+import "fmt"
+
+//func main() {
+//	p1 := test_one.P1{}
+//	p1.SayP2()
+//	p2 := test_two.NewP2(p1)
+//	p2.P.SayP()
+//}
+
+type m[T int | string | float64] map[T]interface{}
 
 //go:generate go run main.go
 func main() {
-	p1 := test_one.P1{}
-	p1.SayP2()
-	p2 := test_two.NewP2(p1)
-	p2.P.SayP()
+	var ts = make(m[string], 10)
+	ts["test"] = 1
+	ts["test2"] = 2
+	fmt.Println(ts)
 }
