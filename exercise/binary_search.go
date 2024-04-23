fa50d457738fbbac1ce73fd90b204b161bb1b7ae
chen.hao <CharlseChen@users.noreply.github.com>
2024-04-23 21:53:14 +0800

    二分查找

--- exercise/binary_search.go
+++ exercise/binary_search.go
@@ -2,12 +2,12 @@ package exercise
 
 func BinarySearch(nums []int, target int) int {
 	i, j := 0, len(nums)-1
-	for i < j {
+	for i <= j {
 		m := i + (j-i)/2
 		if nums[m] < target {
-			j = m + 1
-		} else if nums[m] < target {
-			i = m - 1
+			j = m - 1
+		} else if nums[m] > target {
+			i = m + 1
 		} else {
 			return m
 		}
