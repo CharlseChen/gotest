ef765cf79c0e75909b26de46dd2003fd2a5c5a5e
chen.hao <CharlseChen@users.noreply.github.com>
2024-08-28 09:25:55 +0800

    点赞数据锁粒度小点

--- errgroup_test.go
+++ errgroup_test.go
@@ -7,20 +7,25 @@ import (
 	"errors"
 )
 
+/*
+在errgroup中，其中一个协程报错，不会立即结束等待状态，会等所有协程都运行完
+*/
 func TestErrgroup(t *testing.T) {
 	var g errgroup.Group
 	g.Go(func() error {
-		time.Sleep(1 * time.Second)
-		return nil
-	})
-	g.Go(func() error {
-		time.Sleep(1 * time.Second)
-		return nil
-	})
-	g.Go(func() error {
-		time.Sleep(1 * time.Second)
+		time.Sleep(time.Second)
 		return errors.New("err1223")
 	})
+	g.Go(func() error {
+		i := 0
+		for i < 20 {
+			i++
+			t.Log(i)
+			time.Sleep(time.Second)
+		}
+		return nil
+	})
+
 	if err := g.Wait(); err != nil {
 		t.Fatal(err.Error())
 	}
