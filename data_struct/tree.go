bf1271fdb4bae30cb14f21dbaaa7538b09eb2a0c
chen.hao <CharlseChen@users.noreply.github.com>
2024-10-22 17:51:32 +0800

    合并接口

--- data_struct/tree.go
+++ data_struct/tree.go
@@ -123,3 +123,67 @@ func reverse(a []int) {
 		l, r = l+1, r-1
 	}
 }
+
+func (t *TreeNode) PreTravelV3(root *TreeNode) []int {
+	if root == nil {
+		return []int{}
+	}
+	var stack = list.New() //栈
+	var res []int          //结果集
+	stack.PushBack(root)
+	var node *TreeNode
+	for stack.Len() > 0 {
+		e := stack.Back()
+		stack.Remove(e)     //弹出元素
+		if e.Value == nil { // 如果为空，则表明是需要处理中间节点
+			e = stack.Back() //弹出元素（即中间节点）
+			stack.Remove(e)  //删除中间节点
+			node = e.Value.(*TreeNode)
+			res = append(res, node.Val) //将中间节点加入到结果集中
+			continue                    //继续弹出栈中下一个节点
+		}
+		node = e.Value.(*TreeNode)
+		//压栈顺序：右左中
+		if node.Right != nil {
+			stack.PushBack(node.Right)
+		}
+		if node.Left != nil {
+			stack.PushBack(node.Left)
+		}
+		stack.PushBack(node) //中间节点压栈后再压入nil作为中间节点的标志符
+		stack.PushBack(nil)
+	}
+	return nil
+}
+
+func (t *TreeNode) MidTravelV3(root *TreeNode) []int {
+	if root == nil {
+		return []int{}
+	}
+	var stack = list.New() //栈
+	var res []int          //结果集
+	stack.PushBack(root)
+	var node *TreeNode
+	for stack.Len() > 0 {
+		e := stack.Back()
+		stack.Remove(e)     //弹出元素
+		if e.Value == nil { // 如果为空，则表明是需要处理中间节点
+			e = stack.Back() //弹出元素（即中间节点）
+			stack.Remove(e)  //删除中间节点
+			node = e.Value.(*TreeNode)
+			res = append(res, node.Val) //将中间节点加入到结果集中
+			continue                    //继续弹出栈中下一个节点
+		}
+		node = e.Value.(*TreeNode)
+		//压栈顺序：右左中
+		if node.Right != nil {
+			stack.PushBack(node.Right)
+		}
+		if node.Left != nil {
+			stack.PushBack(node.Left)
+		}
+		stack.PushBack(node) //中间节点压栈后再压入nil作为中间节点的标志符
+		stack.PushBack(nil)
+	}
+	return nil
+}
